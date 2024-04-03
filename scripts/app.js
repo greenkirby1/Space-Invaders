5fb01fb599e7cb5508219bee5e42b42b29715849
Cassie <cassie.lee1023@gmail.com>
2024-04-03 12:15:33 +0100

    wrote function and listener event for player movement left/right

--- scripts/app.js
+++ scripts/app.js
@@ -14,31 +14,31 @@
 
 // ? Constant variables
 const grid = document.querySelector('.game-grid')
-const startPosition = 297
+const startPos = 297
 
 // ? Generate game grid
 const cols = 17
 const rows = 18
-const cellCount = cols * rows
+const blkCount = cols * rows
 
-const cells = []
+const blks = []
 
 function makeGrid() {
-    for (let i = 0; i < cellCount; i++) {
-        const cell = document.createElement('div')
-        cell.innerText = i // delete when done
-        cell.style.width = `${100 / cols}%`
-        cell.style.height = `${100 / rows}%`
-        cells.push(cell)
-        cells.forEach((cell) => {
-            const cellId = `${cells.indexOf(cell)}`
-            return cell.dataset.id = cellId
+    for (let i = 0; i < blkCount; i++) {
+        const blk = document.createElement('div')
+        blk.innerText = i 
+        blk.style.width = `${100 / cols}%`
+        blk.style.height = `${100 / rows}%`
+        blks.push(blk)
+        blks.forEach((blk) => {
+            const blkId = `${blks.indexOf(blk)}`
+            return blk.dataset.id = blkId
         })    
-        grid.append(cell)
-        // console.log(cells)
-        if (i === startPosition) {
-            cell.classList.add('player')
-            // console.log(cells[297])
+        grid.append(blk)
+        // console.log(blks)
+        if (i === startPos) {
+            blk.classList.add('player')
+            // console.log(blks[297])
         }
     }
 }
@@ -48,6 +48,14 @@ makeGrid()
 // ? State changing variables
 let lives
 let score
+let playerCurrPos = blks.indexOf(document.querySelector('.player'))
+let invadersStartPos = [
+    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+    [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
+    [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64],
+    [71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]
+]
 
 // - invaders state
 //   - array with nums specificing starting positions of invaders
@@ -66,7 +74,7 @@ let score
 // - laser from invader
 //   - laser is generated only by invaders at the top row at random setInterval()
 //   - travels down column it was generated in 
-//   - if (cells with .invader-laser && .player === true) {
+//   - if (blks with .invader-laser && .player === true) {
 //          - element.classList.add('explosion')  // css background-image: explosion
 //          - element.classList.remove('invader-laser player')
 //          - player lose one life (lives--)
@@ -79,71 +87,91 @@ let score
 //     }
 
 // - laser from player
-//   - laser travels up the column from cell where it was generated/initiated
-//   - if (cells with .invader-laser > 4) {
-//          - disable space bar key shooting laser
-//          - check whether cells with .invader laser > 4
-//          - enable laser space bar key if condition not met
-//     }
-//   - if (cells with .player-laser && .invader === true) {
-//          - element.classList.add('explosion')
-//          - element.classList.remove('invader-laser invader')
-//          - score(sum of total score) += 100
-//     }
+//   - laser travels up the column from blk where it was generated/initiated
 
 
 // ~ setInterval time decrement
 //   ~ for (invaders row change === true) {
-//          ~ interval++
-//   ~ }
-
-// ~ leaderboard
-//   ~ leaderboard using localStorage
-
-
-// ? Cached elements
-// - start game button
-//   - const startBtn = document.getElementById('start')
-// - pause/resume button
-//   - const pauseBtn = document.getElementById('pause')
-// - play again button
-//   - const playAgainBtn = document.getElementById('play-again')
-// ~ mute button
-//   ~ mute audio when audio is playing
-//   ~ when audio is muted it can be unmuted
-//   ~ const muteBtn = document.getElementById('mute')
-
-
-// ? Event listners
-// document.addEventListner('keydown', playerMove)
-// document.addEventListener('keyup', playerShoot)
-// startBtn.addEventListner('click', init)
-// pauseBtn.addEventListener('click', pause)
-// playAgainBtn.addEventListner('click', init)
-// ~ muteBtn.addEventListener('click', soundOff)
-
-
-
-// ? Functions
-
-
-function init() {
-    makeGrid()
-    // delay for 2000ms then start game
-}
-
-function pause() {
-
-}
-
-function soundOff() {
-
-}
-
-function playerMove(evt) {
-    let currPosition 
-}
-
-function playerShoot() {
-
-}
+    //          ~ interval++
+    //   ~ }
+    
+    // ~ leaderboard
+    //   ~ leaderboard using localStorage
+    
+    
+    // ? Cached elements
+    // - start game button
+    //   - const startBtn = document.getElementById('start')
+    // - pause/resume button
+    //   - const pauseBtn = document.getElementById('pause')
+    // - play again button
+    //   - const playAgainBtn = document.getElementById('play-again')
+    // ~ mute button
+    //   ~ mute audio when audio is playing
+    //   ~ when audio is muted it can be unmuted
+    //   ~ const muteBtn = document.getElementById('mute')
+    
+    
+    // ? Event listners
+    document.addEventListener('keydown', playerMove)
+    document.addEventListener('keyup', playerShoot)
+    // startBtn.addEventListener('click', init)
+    // pauseBtn.addEventListener('click', pause)
+    // playAgainBtn.addEventListener('click', init)
+    // ~ muteBtn.addEventListener('click', soundOff)
+    
+    
+    
+    // ? Functions
+    
+    
+    function init() {
+        makeGrid()
+        // delay for 2000ms then start game
+    }
+    
+    function pause() {
+        
+    }
+    
+    function soundOff() {
+        
+    }
+    
+    function playerMove(evt) {
+        blks[playerCurrPos].classList.remove('player')
+        if (evt.key === 'ArrowLeft' && playerCurrPos !== 289) {
+            playerCurrPos--
+        } else if (evt.key === 'ArrowRight' && playerCurrPos !== 305) {
+            playerCurrPos++
+        } else {
+            console.log("Not Allowed")
+        }
+        // console.log(playerCurrPos)
+        blks[playerCurrPos].classList.add('player')
+    }
+    
+    function playerShoot(evt) {
+        // console.log(evt.code)
+        let shootOrigin = playerCurrPos - 17
+        // console.log(shootOrigin)
+        if (evt.code === 'Space') {
+            blks[shootOrigin].classList.add('swat')
+            for (let i = shootOrigin; i > playerCurrPos - 289; i -= 17) {
+                
+            }
+        }
+        // - to go up column is += 17
+        // - shootOrigin[playerCurrPos + 17]
+        //   - if (blks with .invader-laser > 4) {
+        //          - disable space bar key shooting laser
+        //          - check whether blks with .invader laser > 4
+        //          - enable laser space bar key if condition not met
+        //     }
+        //   - if (blks with .player-laser && .invader === true) {
+        //          - element.classList.add('explosion')
+        //          - element.classList.remove('invader-laser invader')
+        //          - score(sum of total score) += 100
+        //     }  
+    }
+    
